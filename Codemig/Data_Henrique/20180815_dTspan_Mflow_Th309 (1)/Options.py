79b48655fe858cab436ef973a8799f81d09ca3fd
gfidelisp <guilherme.peixer@polo.ufsc.br>
2019-03-20 17:54:39 -0300

    'Newata'

---
+++ Codemig/Data_Henrique/20180815_dTspan_Mflow_Th309 (1)/Options.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 19 14:56:02 2018
+
+@author: Breno
+"""
+# Numero de Ciclos
+no_cycles = 20
+
+# Aquisição de dados
+points_per_cycle = 300
+
+# frequencia de aquisição de dados
+sampling_freq = 150
+
+# Fracao de escoamento CB
+CB_frac = 0.5 #[%]
+
+# Fracao de escoamento HB
+HB_frac = 0.5 #[%]
+
+# Numero de polos magnéticos
+no_polos = 2
+
+# diametro do casing
+D_casing = 24.3 #24.3    #[mm]
+
+# comprimento do casing
+L_casing = 50 #75    #[mm]
+
+# dimensao das esferas
+D_esf = 0.5 #0.3625    #[mm]
+
+# massa total
+m_esf = 79 #[g]
+
+# etilenoglicol
+EG = 0 #[%]
+
+#frequency
+
+frequency=0.5 #[Hz]
